7c3b1a8e9441f30df602450da2ef90c628c712a9
riko123riko123 <56554387+riko123riko123@users.noreply.github.com>
2023-06-07 19:58:25 +0300

    last

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -13,12 +13,14 @@
             біз сізге сәйкес келетін ең жақсысын таңдауға көмектесуге
             қуаныштымыз
           </p>
-          <button
-            type="button"
-            class="mt-[25px] text-white bg-[#573217] hover:bg-[#573217] focus:ring-4 focus:ring-blue-300 font-medium rounded-[25px] text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-[100px]"
+          <router-link to="/Catalog">
+            <button
+              type="button"
+              class="mt-[25px] text-white bg-[#573217] hover:bg-[#573217] focus:ring-4 focus:ring-blue-300 font-medium rounded-[25px] text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-[100px]"
+            >
+              Каталог
+            </button></router-link
           >
-            Каталог
-          </button>
         </div>
         <img
           class="w-[500px]"
